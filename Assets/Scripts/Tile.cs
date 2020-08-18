8ae79c8246489ea60385b42ce5067556029052b3
Andrew Lam <drewclam@hotmail.com>
2020-08-17 23:01:10 -0400

    Add tile

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+    public Sprite flipPlaceholder;
+    Image image;
+
+    private void Awake() {
+        image = GetComponent<Image>();
+    }
+
+    public void Click() {
+        // flip
+        image.sprite = flipPlaceholder;
+        // emit event that tile has been selected
+    }
+}
